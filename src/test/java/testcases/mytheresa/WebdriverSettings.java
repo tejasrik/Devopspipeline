adfd86ea86c06c48ff724fcdaf779d9202e1e100
Sanskar Modi <50732690+Sanskar-Modi@users.noreply.github.com>
2020-06-08 16:00:02 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe File PATH");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Sanskar Modi/Downloads/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
