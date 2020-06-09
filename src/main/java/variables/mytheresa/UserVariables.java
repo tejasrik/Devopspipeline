c943c4a26347c58ecb464b964e5198add712377f
Sanskar Modi <50732690+Sanskar-Modi@users.noreply.github.com>
2020-06-09 10:49:20 +0530

    Update UserVariables.java

--- src/main/java/variables/mytheresa/UserVariables.java
+++ src/main/java/variables/mytheresa/UserVariables.java
@@ -1,10 +1,10 @@
 package variables.mytheresa;
 
 public final class UserVariables {
-	public static final String USER_FIRST_NAME = "First Name";
-	public static final String USER_LAST_NAME = "Last Name";
-	public static final String USER_EMAIL = "example@gmail.com";
-	public static final String USER_PASSWORD = "123456pass";
-	public static final String USER_NEW_PASSWORD = "pass123456";
+	public static final String USER_FIRST_NAME = "Sanskar";
+	public static final String USER_LAST_NAME = "Modi";
+	public static final String USER_EMAIL = "sanskarmodi49@gmail.com";
+	public static final String USER_PASSWORD = "sanskar123";
+	public static final String USER_NEW_PASSWORD = "sanskar123456";
 	
 }
