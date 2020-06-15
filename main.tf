949a0f10206fc0d73da80b743799802d6ea12b38
Sanskar Modi <50732690+Sanskar-Modi@users.noreply.github.com>
2020-06-15 11:55:21 +0530

    Uploaded Terraform files

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "${var.aws_region}"
+  access_key = "AKIAVYQAGAIF2VBHB552"
+  secret_key = "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"
+}
+
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+}
+
+resource "aws_s3_bucket" "tf_code" {
+  bucket = "${var.project_name}-${random_id.tf_bucket_id.dec}"
+  acl    = "private"
+
+  force_destroy = true
+
+  tags {
+    Name = "tf_bucket"
+  }
+}
